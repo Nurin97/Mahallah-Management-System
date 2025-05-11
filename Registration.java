aa5d6bda98c65d7345f7ce37cea8d01e6e0b98b7
ammarazwari <ammarazwari@gmail.com>
2025-05-11 18:21:45 +0800

    Update Registration.java

--- Registration.java
+++ Registration.java
@@ -24,79 +24,80 @@ public class Registration
     private ArrayList<Student> studentList;
     private ArrayList<Staff> staffList;
 
-    public Registration() 
-    {
-        studentList = new ArrayList<>();
-        staffList = new ArrayList<>();
+   public Registration() 
+{
+    studentList = new ArrayList<>();
+    staffList = new ArrayList<>();
 
-        // student
-        studentList.add(new Student("Ammar Azwari", 12345, "ammar@mail.com", 12345678, "Computer Science"));
-        studentList.add(new Student("Ali Umar", 12346, "ali@mail.com", 12345679, "Electrical Engineering"));
-        studentList.add(new Student("Fatimah Zahra", 12347, "fatimah@mail.com", 12345680, "Mechanical Engineering"));
+    // student
+    studentList.add(new Student("Ammar Johari", 2415327, "ammarjohari@mail.com", 013656, "Computer Science", "Faruq"));
+    studentList.add(new Student("Ali Umar", 2415234, "aliomar@mail.com", 12345679, "Electrical Engineering", "Uthman"));
+    studentList.add(new Student("Fatimah Zahra", 2415725, "fatimah@mail.com", 12345680, "Mechanical Engineering", "Aminah"));
 
-        // staff
-        staffList.add(new Staff("Dr. John Doe", 56789, "johndoe@mail.com", 98765432, "Lecturer"));
-        staffList.add(new Staff("Prof. Sarah Ali", 56790, "sarah@mail.com", 98765433, "Professor"));
-        staffList.add(new Staff("Mr. Michael Lee", 56791, "michael@mail.com", 98765434, "Administrator"));
-    }
+    // staff
+    staffList.add(new Staff("Dr. John Doe", 2337134, "johndoe@mail.com", 98765432, "Principal", "Siddiq"));
+    staffList.add(new Staff("Prof. Sarah Ali", 2381430, "sarah@mail.com", 98765433, "Professor", "Aminah"));
+    staffList.add(new Staff("Mr. Abu Bakar", 2344100, "abubako@mail.com", 98765434, "Administrator", "Salahuddin"));
+}
+
     
     //method register
-    public void registerMenu(Scanner sc) 
+   public void registerMenu(Scanner sc) 
+{
+    System.out.println("Registration");
+
+    String mahallahName = Mahallah.chooseMahallah(sc);
+    String block = Mahallah.chooseBlock(sc);
+    String floor = Mahallah.chooseFloor(sc);
+    int roomNum = Room.chooseRoom(sc);
+    String compartment = Room.chooseCompartment(sc);
+    sc.nextLine();  
+
+    System.out.println();
+    System.out.println("Is this student or staff? ");
+    System.out.println("1 - student");
+    System.out.println("2 - staff");
+    int type = sc.nextInt();
+    sc.nextLine(); 
+
+    System.out.println("Enter name: ");
+    String name = sc.nextLine();
+    
+    System.out.println("Enter matric number: ");
+    int id = sc.nextInt();
+    sc.nextLine(); 
+    
+    System.out.println("Enter email: ");
+    String email = sc.nextLine();
+    
+    System.out.println("Enter phone number: ");
+    int phone = sc.nextInt();
+    sc.nextLine(); 
+
+    System.out.println();
+    System.out.print("Enter Kulliyah: ");
+    String position = sc.nextLine();  
+
+    if (type == 1) // student
     {
-        System.out.println("Registration");
-
-        String mahallahName = Mahallah.chooseMahallah(sc);
-        String block = Mahallah.chooseBlock(sc);
-        String floor = Mahallah.chooseFloor(sc);
-        int roomNum = Room.chooseRoom(sc);
-        String compartment = Room.chooseCompartment(sc);
-        sc.nextLine();  
-
-        System.out.println();
-        System.out.println("Is this student or staff? ");
-        System.out.println("1 - student");
-        System.out.println("2 - staff");
-        int type = sc.nextInt();
-        sc.nextLine(); 
-        
-        System.out.println("Enter name: ");
-        String name = sc.nextLine();
-        
-        System.out.println("Enter matric number: ");
-        int id = sc.nextInt();
-        sc.nextLine();  
-        
-        System.out.println("Enter email: ");
-        String email = sc.nextLine();
-        
-        System.out.println("Enter phone number: ");
-        int phone = sc.nextInt();
-        sc.nextLine(); 
-
-        System.out.println();
-        System.out.print("Enter Kulliyah: ");
-        String position = sc.nextLine();  
-
-        if (type == 1) //student
-        {
-            Student s = new Student(name, id, email, phone, position);
-            studentList.add(s);
-        } 
-        else if (type == 2) //staff
-        {
-            Staff staff = new Staff(name, id, email, phone, position);
-            staffList.add(staff);
-        } 
-        else 
-        {
-            System.out.println("Invalid type.");
-        }
-
-        //register message
-        System.out.println("Registration complete!");
-        System.out.println("Welcome, " + name + "!");
-        System.out.println("Mahallah: " + mahallahName + ", Block " + block + ", Floor " + floor + ", Room " + roomNum + ", Compartment: " + compartment);
+        Student s = new Student(name, id, email, phone, position, mahallahName);  // Pass Mahallah
+        studentList.add(s);
+    } 
+    else if (type == 2) // staff
+    {
+        Staff staff = new Staff(name, id, email, phone, position, mahallahName);  // Pass Mahallah
+        staffList.add(staff);
+    } 
+    else 
+    {
+        System.out.println("Invalid type.");
     }
+
+    System.out.println("Registration complete!");
+    System.out.println("Welcome, " + name + "!");
+    System.out.println("Mahallah: " + mahallahName + ", Block " + block + ", Floor " + floor + ", Room " + roomNum + ", Compartment: " + compartment);
+}
+
 
     
     //method remove
